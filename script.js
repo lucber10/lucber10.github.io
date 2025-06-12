5bc1bfac8fff2c9b88b76a0cc5178344d2c54a28
lucber10 <lucber10@wangelev.no>
2025-06-12 13:22:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const videoContainer = document.querySelector('.video-container');
     const video = document.getElementById('hero-video');
     const contentWrapper = document.querySelector('.content-wrapper');
+    const header = document.querySelector('header'); // Get the header element
 
     const videoHeight = videoContainer.offsetHeight;
 
@@ -14,9 +15,11 @@ document.addEventListener('DOMContentLoaded', function() {
         if (scrollY > videoHeight) {
             videoContainer.classList.add('scrolled');
             contentWrapper.style.marginTop = '0'; // Reset margin when scrolled
+            header.classList.add('scrolled'); // Add scrolled class to header
         } else {
             videoContainer.classList.remove('scrolled');
             contentWrapper.style.marginTop = `${videoHeight - scrollY}px`;
+            header.classList.remove('scrolled'); // Remove scrolled class from header
         }
     });
 });
